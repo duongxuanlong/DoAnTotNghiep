41be1e850450343c5a29fbdd75f8a9fcd4dd6ae7
duongxuanlong <xuanlong.8888@gmail.com>
2016-10-25 22:32:39 +0700

    * Dan Y cho luan van

---
+++ Thesis-Template/DanY.mm
@@ -0,0 +1,64 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1477407866531" ID="ID_1100026632" MODIFIED="1477409037756" TEXT="N&#x1ed9;i dung lu&#x1ead;n v&#x103;n">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1477408172743" HGAP="46" ID="ID_89435913" MODIFIED="1477409411510" POSITION="left" TEXT="Ch&#x1b0;&#x1a1;ng I: Gi&#x1edb;i thi&#x1ec7;u" VSHIFT="-9">
+<node CREATED="1477408338191" HGAP="61" ID="ID_1001220100" MODIFIED="1477409023716" TEXT="Gi&#x1edb;i thi&#x1ec7;u gom nh&#xf3;m v&#x103;n b&#x1ea3;n" VSHIFT="-31"/>
+<node CREATED="1477408364649" HGAP="62" ID="ID_4215914" MODIFIED="1477408461276" TEXT="L&#x1ee3;i &#xed;ch v&#xe0; t&#x1ea7;m quan tr&#x1ecd;ng c&#x1ee7;a gom nh&#xf3;m v&#x103;n b&#x1ea3;n" VSHIFT="-34"/>
+<node CREATED="1477408407917" HGAP="65" ID="ID_1665779842" MODIFIED="1477408458205" TEXT="Nh&#x1eef;ng th&#xe1;ch th&#x1ee9;c khi l&#xe0;m gom nh&#xf3;m v&#x103;n b&#x1ea3;n" VSHIFT="-18"/>
+<node CREATED="1477408428252" HGAP="57" ID="ID_1742287597" MODIFIED="1477408455329" TEXT="C&#xe1;c ph&#x1b0;&#x1a1;ng ph&#xe1;p gom nh&#xf3;m v&#x103;n b&#x1ea3;n hi&#x1ec7;n th&#x1edd;i" VSHIFT="26"/>
+</node>
+<node CREATED="1477408505211" HGAP="102" ID="ID_1674197702" MODIFIED="1477409387246" POSITION="right" TEXT="Ch&#x1b0;&#x1a1;ng II: C&#xe1;c c&#xf4;ng tr&#xec;nh li&#xea;n quan" VSHIFT="2">
+<node CREATED="1477408527654" ID="ID_1868226884" MODIFIED="1477408535092" TEXT="C&#xe1;c ph&#x1b0;&#x1a1;ng ph&#xe1;p gom nh&#xf3;m">
+<node CREATED="1477408539372" ID="ID_1835913605" MODIFIED="1477408550352" TEXT="K&#x1ec3; ra m&#x1ed9;t v&#xe0;i ph&#x1b0;&#x1a1;ng ph&#xe1;p gom nh&#xf3;m"/>
+</node>
+<node CREATED="1477408565108" ID="ID_885634644" MODIFIED="1477408574844" TEXT="Ph&#x1b0;&#x1a1;ng ph&#xe1;p gom nh&#xf3;m ph&#xe2;n c&#x1ea5;p">
+<node CREATED="1477408579808" ID="ID_1150828163" MODIFIED="1477408586794" TEXT="C&#xe1;c h&#x1b0;&#x1edb;ng ti&#x1ebf;p c&#x1ead;n">
+<node CREATED="1477408590707" ID="ID_1754208670" MODIFIED="1477408601789" TEXT="H&#x1b0;&#x1edb;ng ti&#x1ebf;p c&#x1ead;n agglomerative">
+<node CREATED="1477408606123" ID="ID_665791555" MODIFIED="1477408619013" TEXT="Gi&#x1edb;i thi&#x1ec7;u v&#x1ec1; h&#x1b0;&#x1edb;ng ti&#x1ebf;p c&#x1ead;n agglomerative"/>
+<node CREATED="1477408622602" ID="ID_319749985" MODIFIED="1477408633641" TEXT="V&#xed; d&#x1ee5; v&#x1ec1; agglomerative"/>
+</node>
+<node CREATED="1477408640342" ID="ID_1653779075" MODIFIED="1477408646897" TEXT="H&#x1b0;&#x1edb;ng ti&#x1ebf;p c&#x1ead;n divisive">
+<node CREATED="1477408649936" ID="ID_64087766" MODIFIED="1477408661082" TEXT="Gi&#x1edb;i thi&#x1ec7;u v&#x1ec1; h&#x1b0;&#x1edb;ng ti&#x1ebf;p c&#x1ead;n divisive"/>
+<node CREATED="1477408665354" ID="ID_1335739567" MODIFIED="1477408670413" TEXT="V&#xed; d&#x1ee5; v&#x1ec1; divisive"/>
+</node>
+</node>
+</node>
+<node CREATED="1477408682287" ID="ID_714643691" MODIFIED="1477408690111" TEXT="&#x110;&#x1ed3;ng gom nh&#xf3;m ph&#xe2;n c&#x1ea5;p">
+<node CREATED="1477408701179" ID="ID_1556392640" MODIFIED="1477408706129" TEXT="Gi&#x1edb;i thi&#x1ec7;u">
+<node CREATED="1477408709487" ID="ID_1295820272" MODIFIED="1477408734473" TEXT="&#x110;i&#x1ec3;m h&#x1ea1;n ch&#x1ebf; v&#x1ec1; gom nh&#xf3;m ph&#xe2;n c&#x1ea5;p"/>
+<node CREATED="1477408748842" ID="ID_1029942883" MODIFIED="1477408756525" TEXT="Gi&#x1edb;i thi&#x1ec7;u v&#x1ec1; &#x111;&#x1ed3;ng gom nh&#xf3;m"/>
+<node CREATED="1477408760966" ID="ID_571658830" MODIFIED="1477408773507" TEXT="S&#x1ef1; k&#x1ebf;t h&#x1ee3;p v&#x1ec1; &#x111;&#x1ed3;ng gom nh&#xf3;m ph&#xe2;n c&#x1ea5;p"/>
+<node CREATED="1477408784565" ID="ID_653444332" MODIFIED="1477408798503" TEXT="C&#xf4;ng d&#x1ee5;ng c&#x1ee7;a &#x111;&#x1ed3;ng gom nh&#xf3;m ph&#xe2;n c&#x1ea5;p"/>
+<node CREATED="1477408803794" ID="ID_239863757" MODIFIED="1477408816828" TEXT="Gi&#x1ea3;i th&#xed;ch vi&#x1ec7;c t&#x1b0;&#x1a1;ng t&#xe1;c hai chi&#x1ec1;u c&#x1ee7;a d&#x1eef; li&#x1ec7;u"/>
+<node CREATED="1477408820744" ID="ID_615022848" MODIFIED="1477408830842" TEXT="K&#x1ebf;t lu&#x1ead;n v&#x1ec1; &#x111;&#x1ed3;ng gom nh&#xf3;m ph&#xe2;n c&#x1ea5;p"/>
+</node>
+<node CREATED="1477408837505" ID="ID_671530893" MODIFIED="1477408848933" TEXT="&#x110;&#x1ed9; t&#x1b0;&#x1a1;ng &#x111;&#x1ed3;ng Goodman Kruskal">
+<node CREATED="1477408853997" ID="ID_564552418" MODIFIED="1477408863978" TEXT="V&#xec; sao ch&#x1ecd;n Goodman kruskal"/>
+<node CREATED="1477408867672" ID="ID_338408168" MODIFIED="1477408877784" TEXT="Gi&#x1edb;i thi&#x1ec7;u v&#x1ec1; Goodman kruskal"/>
+<node CREATED="1477408887997" ID="ID_1903723072" MODIFIED="1477408897886" TEXT="Lu&#x1ead;t s&#x1eed; d&#x1ee5;ng Goodman kruskal"/>
+<node CREATED="1477408902765" ID="ID_750512912" MODIFIED="1477408912368" TEXT="V&#xed; d&#x1ee5; v&#x1ec1; Goodman kruskal"/>
+</node>
+<node CREATED="1477408926689" ID="ID_994367044" MODIFIED="1477408931662" TEXT="Thu&#x1ead;t to&#xe1;n">
+<node CREATED="1477408934070" ID="ID_747261450" MODIFIED="1477408939865" TEXT="Gi&#x1edb;i thi&#x1ec7;u thu&#x1ead;t to&#xe1;n"/>
+<node CREATED="1477408943162" ID="ID_220306751" MODIFIED="1477408946680" TEXT="C&#xe1;c gi&#x1ea3;i thu&#x1ead;t"/>
+</node>
+</node>
+</node>
+<node CREATED="1477409051741" HGAP="56" ID="ID_271002728" MODIFIED="1477409380529" POSITION="right" TEXT="Ch&#x1b0;&#x1a1;ng III: Ph&#x1b0;&#x1a1;ng ph&#xe1;p &#x111;&#x1ec1; xu&#x1ea5;t" VSHIFT="-58">
+<node CREATED="1477409097157" ID="ID_1008761089" MODIFIED="1477409107732" TEXT="&#x110;i&#x1ec3;m h&#x1ea1;n ch&#x1ebf; c&#x1ee7;a thu&#x1ead;t to&#xe1;n"/>
+<node CREATED="1477409114843" ID="ID_394062569" MODIFIED="1477409137750" TEXT="Gi&#x1edb;i thi&#x1ec7;u v&#x1ec1; &#x111;&#x1ed9; &#x111;o cho s&#x1ef1; k&#x1ebf;t h&#x1ee3;p c&#x1ee7;a bi&#x1ebf;n li&#xea;n t&#x1ee5;c"/>
+<node CREATED="1477409141771" ID="ID_790077871" MODIFIED="1477409147641" TEXT="L&#x1ef1;a ch&#x1ecd;n &#x111;&#x1ed9; &#x111;o t&#x1ed1;t nh&#x1ea5;t"/>
+<node CREATED="1477409173597" ID="ID_429107838" MODIFIED="1477409188160" TEXT="&#xc1;p d&#x1ee5;ng v&#xe0;o thu&#x1ead;t to&#xe1;n v&#x1edb;i th&#x1ec3; hi&#x1ec7;n doc2vec cho v&#x103;n b&#x1ea3;n"/>
+</node>
+<node CREATED="1477409230607" HGAP="58" ID="ID_705310662" MODIFIED="1477409371576" POSITION="right" TEXT="Ch&#x1b0;&#x1a1;ng IV: Th&#x1ef1;c nghi&#x1ec7;m v&#xe0; k&#x1ebf;t qu&#x1ea3;" VSHIFT="-7">
+<node CREATED="1477409253390" ID="ID_1563707809" MODIFIED="1477409264557" TEXT="Gi&#x1edb;i thi&#x1ec7;u v&#x1ec1; d&#x1eef; li&#x1ec7;u ti&#x1ebf;ng Anh"/>
+<node CREATED="1477409267034" ID="ID_952857824" MODIFIED="1477409278195" TEXT="Gi&#x1edb;i thi&#x1ec7;u v&#x1ec1; d&#x1eef; li&#x1ec7;u ti&#x1ebf;ng Vi&#x1ec7;t"/>
+<node CREATED="1477409281308" ID="ID_583783054" MODIFIED="1477409286525" TEXT="K&#x1ebf;t qu&#x1ea3; th&#x1ef1;c nghi&#x1ec7;m"/>
+</node>
+<node CREATED="1477409315202" HGAP="34" ID="ID_615355025" MODIFIED="1477409366869" POSITION="right" TEXT="Ch&#x1b0;&#x1a1;ng V: K&#x1ebf;t lu&#x1ead;n v&#xe0; h&#x1b0;&#x1edb;ng ph&#xe1;t tri&#x1ec3;n" VSHIFT="85">
+<node CREATED="1477409334910" ID="ID_266357815" MODIFIED="1477409343738" TEXT="K&#x1ebf;t qu&#x1ea3; &#x111;&#x1ea1;t &#x111;&#x1b0;&#x1ee3;c"/>
+<node CREATED="1477409347438" ID="ID_711088187" MODIFIED="1477409353739" TEXT="&#x110;&#xf3;ng g&#xf3;p m&#x1edb;i cho b&#xe0;i to&#xe1;n"/>
+</node>
+</node>
+</map>
