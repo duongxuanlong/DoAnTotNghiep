c734890cc2724b33cb40f9042366669aa8f67a03
duongxuanlong <xuanlong.8888@gmail.com>
2016-12-29 06:15:54 +0700

    * Implemnet doc2vec

---
+++ ClusterStat/Expressions/ExD2V.py
@@ -0,0 +1,16 @@
+__author__ = 'Long'
+
+import gensim
+from gensim.models.doc2vec import Doc2Vec
+
+
+
+class DocIterator:
+    def __init__(self, doclist, labellist):
+        self.DocList = doclist
+        self.LabelList = labellist
+
+    def __iter__(self):
+        for idx, doc in enumerate(self.DocList):
+            yield gensim.models.doc2vec.LabeledSentence(words=doc.split(), labels=[self.LabelList[idx]])
+
